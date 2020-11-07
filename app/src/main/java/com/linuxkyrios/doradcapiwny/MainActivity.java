21f0f31c687a4be921fcd7ebe16256de36bb10c4
Linux Kyrios <malinowskim92@protonmail.com>
2020-11-07 19:02:31 +0100

    Created simple activity for application connected with button

--- app/src/main/java/com/linuxkyrios/doradcapiwny/MainActivity.java
+++ app/src/main/java/com/linuxkyrios/doradcapiwny/MainActivity.java
@@ -1,14 +1,28 @@
 package com.linuxkyrios.doradcapiwny;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity; //imported Activity class
+import android.os.Bundle; //imported Bundle class
+import android.view.View; //imported View class
+import android.widget.Spinner;
+import android.widget.TextView;
 
-import android.os.Bundle;
-
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle savedInstanceState){
+        super .onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClickFindBeer(View view){
+        //Downloads the TextView component reference
+        TextView brands = (TextView) findViewById(R.id.brands);
+        // Downloads the Spinner component reference
+        Spinner color = (Spinner) findViewById(R.id.color);
+        // Downloads selected value from Spinner component
+        String beerType = String.valueOf(color.getSelectedItem());
+        // Displaying selected value
+        brands.setText(beerType);
+
     }
 }
